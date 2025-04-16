85e368b3763153965ff5f9788e3d89bf1b9be78f
Brian Cribb <brian.cribb@gmail.com>
2025-04-16 10:28:39 -0400

    Simpler

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Gather from './Gather.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Gather />
-  </StrictMode>,
-)
