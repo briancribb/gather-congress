a13fd12e0cdf0a6a11c77aaaa1838b964916da8b
Brian Cribb <brian.cribb@gmail.com>
2025-04-19 09:22:57 -0400

    Getting reelection years

--- build-transformed-list.js
+++ build-transformed-list.js
@@ -1,7 +1,22 @@
 const fs = require('fs');
 const detailedList = JSON.parse(fs.readFileSync('./data/detailed-members.json', 'utf8'));
+const senatorsClass1 = JSON.parse(fs.readFileSync('./data/senatorsClass1.json', 'utf8'));
+const senatorsClass2 = JSON.parse(fs.readFileSync('./data/senatorsClass2.json', 'utf8'));
+const senatorsClass3 = JSON.parse(fs.readFileSync('./data/senatorsClass3.json', 'utf8'));
 
 
+
+function getNextEvenYear() {
+  const now = new Date();
+  let year = now.getFullYear();
+  while (year % 2 !== 0) {
+    year++;
+  }
+  return year;
+}
+
+const nextEvenYear = getNextEvenYear();
+console.log(nextEvenYear);
 
 const transformList = () => {
 	console.log('transformList()');
@@ -10,8 +25,8 @@ const transformList = () => {
 		let recentTerm = member.terms[member.terms.length-1],
 			recentParty = member.partyHistory[member.partyHistory.length-1];
 
-		return {
-			bioguideId: member.bioguideId,
+		let updatedMember = {
+			id: member.bioguideId,
 			birthYear: member.birthYear,
 			firstName: member.firstName,
 			lastName: member.lastName,
@@ -20,14 +35,36 @@ const transformList = () => {
 			chamber: recentTerm.chamber,
 			type: recentTerm.memberType,
 			district: recentTerm.district,
+			partyName: recentParty.partyName,
+			partyAbbr: recentParty.partyAbbreviation,
 			startYear: recentTerm.startYear,
+			reelectionYear: getNextEvenYear(),
 			stateName: recentTerm.stateName,
 			stateCode: recentTerm.stateCode,
 			type: recentTerm.memberType,
 			phone: member.addressInformation?.phoneNumber,
-			imageUrl: member.depiction?.imageUrl,
-			imageLink: member.depiction?.attribution
+			imageUrl: member.depiction?.imageUrl || "",
+			imageCredit: member.depiction?.attribution || ""
 		}
+
+		if (updatedMember.chamber === "Senate") {
+			if (senatorsClass1.includes(updatedMember.lastNameFirst)) {
+				updatedMember.senateClass = 1;
+				updatedMember.reelectionYear = 2031;
+			} else if (senatorsClass2.includes(updatedMember.lastNameFirst)) {
+				updatedMember.senateClass = 2;
+				updatedMember.reelectionYear = 2027;
+			} else if (senatorsClass3.includes(updatedMember.lastNameFirst)) {
+				updatedMember.senateClass = 3;
+				updatedMember.reelectionYear = 2029;
+			} else {
+				updatedMember.reelectionYear = "MissingYear";
+				console.log(`Mismatched name: ${updatedMember.lastNameFirst}`);
+			}
+		}
+
+		return updatedMember;
+
 	});
 
 
